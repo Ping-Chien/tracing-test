f94142e9036f8398aca078888482de113c82f335
chien-ping <pingchien.biz@gmail.com>
2025-06-22 10:10:52 +0800

    feat: 更新Docker映像檔建置腳本

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# 參數：可自訂 app image tag，預設為 tracing-test:ori
+# 參數：可自訂 app image tag，預設為 tracing-test:push-mode
 APP_TAG=${1:-tracing-test:push-mode}
-# 參數：可自訂 javaagent image tag，預設為 javaagent:2.16.0
-JAVAAGENT_TAG=${2:-javaagent:2.16.0}
 
 # 編譯 Spring Boot 專案
 ./gradlew clean build -x test
@@ -22,16 +20,5 @@ if [ $? -eq 0 ]; then
   echo "Spring Boot app Docker image 已建立，tag: $APP_TAG"
 else
   echo "Spring Boot app Docker build 失敗"
-  exit 1
-fi
-
-# 建立 javaagent image
-
-docker build -f Dockerfile.javaagent -t $JAVAAGENT_TAG .
-
-if [ $? -eq 0 ]; then
-  echo "Javaagent image 已建立，tag: $JAVAAGENT_TAG"
-else
-  echo "Javaagent image 建立失敗"
   exit 1
 fi
