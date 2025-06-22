51482085e214a4eb818c02da75579e0bed440060
chien-ping <pingchien.biz@gmail.com>
2025-06-22 10:08:29 +0800

    chore: 添加vscode配置目錄到gitignore文件中

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# 參數：可自訂 app image tag，預設為 tracing-test:ori
-APP_TAG=${1:-tracing-test:push-mode}
-# 參數：可自訂 javaagent image tag，預設為 javaagent:2.16.0
-JAVAAGENT_TAG=${2:-javaagent:2.16.0}
+# 參數：可自訂 app image tag，預設為 tracing-test:pull-mode
+APP_TAG=${1:-tracing-test:pull-mode}
+
 
 # 編譯 Spring Boot 專案
 ./gradlew clean build -x test
@@ -22,16 +21,5 @@ if [ $? -eq 0 ]; then
   echo "Spring Boot app Docker image 已建立，tag: $APP_TAG"
 else
   echo "Spring Boot app Docker build 失敗"
-  exit 1
-fi
-
-# 建立 javaagent image
-
-docker build -f Dockerfile.javaagent -t $JAVAAGENT_TAG .
-
-if [ $? -eq 0 ]; then
-  echo "Javaagent image 已建立，tag: $JAVAAGENT_TAG"
-else
-  echo "Javaagent image 建立失敗"
   exit 1
 fi
